bab436765c84f916d84463426f8d3425206244e1
Victor-El <victorelezua@gmail.com>
2020-10-21 18:45:54 +0100

    feat: add validations to login activity

--- app/src/main/java/com/mobileedu8/andelaschools/Activity/LoginActivity.java
+++ app/src/main/java/com/mobileedu8/andelaschools/Activity/LoginActivity.java
@@ -1,16 +1,89 @@
 package com.mobileedu8.andelaschools.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.mobileedu8.andelaschools.R;
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
+import com.mobsandgeeks.saripaar.annotation.Password;
 
-public class LoginActivity extends AppCompatActivity {
+import java.util.List;
+
+public class LoginActivity extends AppCompatActivity implements Validator.ValidationListener {
+
+    private Validator validator;
+
+    private ConstraintLayout rootView;
+
+    @Email
+    private EditText emailEditText;
+
+    @Password
+    private EditText passwordEditText;
+
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        setUpValidation();
+
+        initViews();
+
     }
+
+    private void setUpValidation() {
+        validator = new Validator(this);
+        validator.setValidationListener(this);
+    }
+
+    private void initViews() {
+        rootView = findViewById(R.id.login_root_view);
+        emailEditText = findViewById(R.id.email);
+        passwordEditText = findViewById(R.id.password);
+        loginBtn = findViewById(R.id.login_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validator.validate();
+            }
+        });
+    }
+
+    @Override
+    public void onValidationSucceeded() {
+        Toast.makeText(this, "Validation Successful", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+
+        for (ValidationError error: errors) {
+            View view = error.getView();
+
+            if (view instanceof EditText) {
+                ((EditText) view).setError(error.getCollatedErrorMessage(this));
+            }
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        validator = null;
+        super.onDestroy();
+    }
+
 }
